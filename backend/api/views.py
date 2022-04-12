e8b143a9d38acae440809f94bb228fdfee364410
Yash Bhanderi <yashbhanderi1818@gmail.com>
2022-04-12 15:28:49 +0530

    added changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,6 +12,7 @@ class TodoListViewset(viewsets.ModelViewSet):
     # queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
     permission_classes = IsAuthenticated,
+    print("serializer_class")
 
     def get_queryset(self):
         return TodoList.objects.all().order_by('-status', 'priority')
